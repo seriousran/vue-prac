fd5542ccd5be4fc1026723afe98db68951ed448e
seriousmac <bassilver@naver.com>
2018-03-08 18:28:39 +0900

    v1.1.2

--- src/components/Todo/TodoList.vue
+++ src/components/Todo/TodoList.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="index">{{ todoItem }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      todoItems: []
+    }
+  },
+  created () {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i))
+      }
+    }
+  }
 }
 </script>
 

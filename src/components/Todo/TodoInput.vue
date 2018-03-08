63e1f840a981b0c15c9efb17ff0efd9d2fc73e0d
seriousmac <bassilver@naver.com>
2018-03-08 18:23:18 +0900

    v1.1.1

--- src/components/Todo/TodoInput.vue
+++ src/components/Todo/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button v-on:click="addTodo">추가</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    </span>
+    <!--<button v-on:click="addTodo">추가</button>-->
   </div>
 </template>
 
@@ -24,5 +27,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  input:focuse {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>

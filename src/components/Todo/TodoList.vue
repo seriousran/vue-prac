5644c63f6d9e5a5ae28052a33a7f119124d9d135
seriousmac <bassilver@naver.com>
2018-03-08 18:33:42 +0900

    v1.1.3

--- src/components/Todo/TodoList.vue
+++ src/components/Todo/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
   <section>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="index">{{ todoItem }}</li>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="index" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+        </span>
+      </li>
     </ul>
   </section>
 </template>
@@ -24,4 +30,33 @@ export default {
 </script>
 
 <style>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+
 </style>
